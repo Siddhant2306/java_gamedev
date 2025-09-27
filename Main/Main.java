eeae23f6b5c1a3aceb32653e9363832e55739de5
siddhant <siddhant2232sharma2232@gmail.com>
2025-09-27 11:48:13 +0530

    Main window setup

---
+++ Main/Main.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] arg){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("class_Project");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+    }
+}
